0b96cc5e55bdd2d598df456796af4a005e2262af
Arjun Sarode <sarodearjun57@gmail.com>
2015-01-28 00:40:33 -0600

    fade in pics working

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 var allPics = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg", "11.jpg", "12.jpg", "13.jpg", "14.jpg"]
-
-var imgArr = []
-
+var imgArr = [];
+var imgIndex = 0;
 var screenWidth = $(window).width();
 
 $(document).ready(function(){
@@ -16,16 +15,30 @@ var addPic = function(){
 
 	var xPos = Math.floor((Math.random() - .2) * screenWidth);
 	var yPos = Math.floor(Math.random() * 500);
-	var imgWidth = Math.floor(Math.random() * 300);
-	var imgPath = "img/" + allPics[Math.floor(Math.random() * 14)];
+	var imgWidth = 100+ Math.floor(Math.random() * 200);
+	var imgPath = "img/" + allPics[imgIndex];
+	if (imgIndex != allPics.length - 1) {
+		imgIndex++;
+	} else {
+		imgIndex = 0;
+	}
 	
-	var imgElement = $("img");
+	var imgElement = $("<img>");
 	imgElement.attr("src", imgPath);
-	imgElement.css({'width': imgWidth, 'position': 'absolute', 'left': xPos, 'top': yPos});
+	imgElement.css({'z-index': '0', 'display': 'none', 'width': imgWidth, 'position': 'absolute', 'left': xPos, 'top': yPos});
 
 	if(imgArr.length < 5){
 		imgArr.unshift(imgElement);
+		$('.header').append(imgElement);
+		imgElement.fadeIn("slow");
+	} else {
+		imgArr.pop().fadeOut("slow", function() {
+			$(this).remove();
+		});
+		imgArr.unshift(imgElement);
+		$('.header').append(imgElement);
+		imgElement.fadeIn("slow");
 	}
 
-	$('.header').append(imgElement);	
+		
 };
